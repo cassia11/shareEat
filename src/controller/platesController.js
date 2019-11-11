2aeffb38c7bb62e9131d736d968b7b65ff11bd05
cassia11 <cassiagomes0102@gmail.com>
2019-11-10 22:38:42 -0300

    metodo save com caso de sucesso e falha

--- src/controller/platesController.js
+++ src/controller/platesController.js
@@ -1,8 +1,19 @@
 const platesService = require('./../services/platesService')
+const plate = require('./../dominio/plates')
+
 
 module.exports = {
     getPlates: async (req, resp) => {
         const plates = await platesService.getPlates(req.params.id);
         resp.status(200).send(plates);
+    }, 
+
+    save: async (req, res) => {
+    try {
+      await platesService.save(plate);
+      res.status(200).send({message: 'Prato cadastrado com sucesso!'});
+    } catch (e) {
+      res.status(500).send({message: 'Falha ao cadastrar um novo prato.'});
     }
+}
 }
