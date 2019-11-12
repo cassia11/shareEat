d085d8725a44f3c480ccab34d43d0f8992deeabf
cassia11 <cassiagomes0102@gmail.com>
2019-11-12 01:02:28 -0300

    adiciona parseJSON

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-// App
+
 const app = express();
+app.use(bodyParser.json());
 
-// Load routes
 const indexRoutes = require('./routes/index-routes');
 app.use('/', indexRoutes);
 
