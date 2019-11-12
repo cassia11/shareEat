c84843e6d5c91e893ea2ec2fa48ef574ab6fecbd
cassia11 <cassiagomes0102@gmail.com>
2019-11-12 01:09:43 -0300

    remove comentários

--- src/repository/database.js
+++ src/repository/database.js
@@ -2,7 +2,6 @@ var sqlite3 = require('sqlite3').verbose()
 
 const db = new sqlite3.Database('./places.sqlite', (err) => {
     if (err) {
-      // Cannot open database
       console.error(err.message)
       throw err
     }else{
