a019bccee56b9fb142503a4613e912a3cf779a96
cassia11 <cassiagomes0102@gmail.com>
2019-11-17 19:22:23 -0300

    Adiciona na consulta dos lugares a quantidade de pratos de cada um

--- src/repository/placesRepository.js
+++ src/repository/placesRepository.js
@@ -3,7 +3,11 @@ const db = require('./database');
 module.exports = {
     getPlaces: () => {
         return new Promise((resolve, reject) => {
-            db.all("SELECT * FROM places", async (err, rows) => {
+            db.all("SELECT C.ID, C.NAME, COUNT(`PLACES_ID`) AS plates_qtd \
+                    FROM PLACES AS C  \
+                    LEFT JOIN PLATES AS P \
+                    ON C.ID = P.PLACES_ID \
+                    GROUP BY C.ID;", async (err, rows) => {
                 if (err) {
                     throw reject(err);
                 }
